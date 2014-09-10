f39bfa3ac6b85bd954a41e13e044549763bd1ca0
Lynn <sotii@163.com>
2014-09-10 10:59:37 +0800

    Add module
    Signed-off-by: Lynn <sotii@163.com>

---
+++ import_hello.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+print "hello, world!"
+
+def hello():
+	print "hello, lynn"
---
+++ import_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+def hello():
+	print "hello, world"
+
+def test():
+	print "hello, test"
+
+if __name__ == '__main__':
+	test()
